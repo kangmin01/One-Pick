c206540eaac287f326e0425d5bcf525a3d1d4616
kangmin01 <mineek99@gmail.com>
2022-11-03 17:47:33 +0900

    feat : create start page

---
+++ src/views/start/start.js
@@ -0,0 +1,5 @@
+const startYes = document.querySelector(".start-yes");
+const startNo = document.querySelector(".start-no");
+
+startYes.addEventListener("click", () => alert("주문 페이지로 이동"));
+startNo.addEventListener("click", () => alert("메인 페이지로 이동"));
